461e0836d27938a57222098c5698c481c4b9cd2c
baimwk <blwkkz@gmail.com>
2019-04-15 16:38:07 +0300

    Update HashTable.py

--- task_3/HashTable.py
+++ task_3/HashTable.py
@@ -1,76 +1,75 @@
-class HashTable:
-    def __init__(self): #имя конструктора класса
+class MyHashTable:
+    def __init__(self):  # имя конструктора класса
         self.size = 11
-        self.slots = [None] * self.size #ключи элементов
-        self.data = [None] * self.size #значения
+        self.slots = [None] * self.size  # ключи элементов
+        self.data = [None] * self.size  # значения
 
-    def put(self, key, data): #Добавляет новую пару ключ-значение в отображение. Если такой ключ уже имеется, то заменяет старое значение новым.
-      hashvalue = self.hashfunction(key, len(self.slots))
+    def put(self, key, data):  # Добавляет новую пару ключ-значение в отображение. Если ключ имеется, то заменяет старый
+        hashvalue = self.hashfunction(key, len(self.slots))
 
-      if self.slots[hashvalue] == None:
-        self.slots[hashvalue] = key
-        self.data[hashvalue] = data
-      else:
-        if self.slots[hashvalue] == key:
-          self.data[hashvalue] = data  #replace
+        if self.slots[hashvalue] is None:
+            self.slots[hashvalue] = key
+            self.data[hashvalue] = data
         else:
-          nextslot = self.rehash(hashvalue,len(self.slots))
-          while self.slots[nextslot] != None and \
-                          self.slots[nextslot] != key:
-            nextslot = self.rehash(nextslot,len(self.slots))
+            if self.slots[hashvalue] == key:
+                self.data[hashvalue] = data  # replace
+            else:
+                nextslot = self.rehash(hashvalue, len(self.slots))
+                while self.slots[nextslot] is not None and self.slots[nextslot] != key:
+                    nextslot = self.rehash(nextslot, len(self.slots))
 
-          if self.slots[nextslot] == None:
-            self.slots[nextslot]=key
-            self.data[nextslot]=data
-          else:
-            self.data[nextslot] = data #replace
+                if self.slots[nextslot] is None:
+                    self.slots[nextslot] = key
+                    self.data[nextslot] = data
+                else:
+                    self.data[nextslot] = data  # replace
 
-    def hashfunction(self,key,size): #простой метод остатков
-         return key%size
+    def hashfunction(self, key, size):  # простой метод остатков
+        return key % size
 
-    def rehash(self,oldhash,size): #линейное пробивание на 1
-        return (oldhash+1)%size
+    def rehash(self, oldhash, size):  # линейное пробивание на 1
+        return (oldhash + 1) % size
 
-    def get(self,key): #Принимает ключ, возвращает соответствующее ему значение из коллекции или None
-      startslot = self.hashfunction(key,len(self.slots))
+    def get(self, key):  # Принимает ключ, возвращает соответствующее ему значение из коллекции или None
+        startslot = self.hashfunction(key, len(self.slots))
 
-      data = None
-      stop = False
-      found = False
-      position = startslot
-      while self.slots[position] != None and  \
-                           not found and not stop:
-         if self.slots[position] == key:
-           found = True
-           data = self.data[position]
-         else:
-           position=self.rehash(position,len(self.slots))
-           if position == startslot:
-               stop = True
-      return data
+        data = None
+        stop = False
+        found = False
+        position = startslot
+        while self.slots[position] is not None and not found and not stop:
+            if self.slots[position] == key:
+                found = True
+                data = self.data[position]
+            else:
+                position = self.rehash(position, len(self.slots))
+                if position == startslot:
+                    stop = True
+        return data
 
-    def __getitem__(self,key):
+    def __getitem__(self, key):
         return self.get(key)
 
-    def __setitem__(self,key,data):
-        self.put(key,data)
+    def __setitem__(self, key, data):
+        self.put(key, data)
 
-H=HashTable()
-H[54]="cat"
-H[26]="dog"
-H[93]="lion"
-H[17]="tiger"
-H[77]="bird"
-H[31]="cow"
-H[44]="goat"
-H[55]="pig"
-H[20]="chicken"
+
+H = HashTable()
+H[54] = "cat"
+H[26] = "dog"
+H[93] = "lion"
+H[17] = "tiger"
+H[77] = "bird"
+H[31] = "cow"
+H[44] = "goat"
+H[55] = "pig"
+H[20] = "chicken"
 print(H.slots)
 print(H.data)
 
 print(H[20])
 
 print(H[17])
-H[20]='duck'
+H[20] = 'duck'
 print(H[20])
 print(H[99])
